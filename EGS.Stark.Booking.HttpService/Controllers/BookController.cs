7f8e05ef4669b7a2d4e01519e7345e771665c450
Mahsa Ghasemzadeh <ghasemzadeh.mahsa@gmail.com>
2016-09-24 18:06:08 +0330

    added get by id method

--- EGS.Stark.Booking.HttpService/Controllers/BookController.cs
+++ EGS.Stark.Booking.HttpService/Controllers/BookController.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using EGS.Stark.Booking.Manager;
+using EGS.Stark.Booking.Model;
+using EGS.Stark.Web.ApiResults;
 
 namespace EGS.Stark.Booking.HttpService.Controllers
 {
-    public class BookController : ApiController
+    [RoutePrefix("book")]
+    public class BookController : Web.ApiController
     {
+        private IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<HttpResponseMessage> GetById(int id, CancellationToken cancellationToken)
+        {
+            return await Ok(new ApiResultModel<Book>()
+            {
+                Data = await _bookService.GetAsyncById(id)
+            }).ExecuteAsync(cancellationToken);
+        }
+
+
     }
 }
